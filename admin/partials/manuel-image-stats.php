51dc6f737ce5c77e1066a0d1d33ae67a7b328b98
thelastpoet <emcie4@gmail.com>
2023-05-13 20:16:14 +0300

    Improved stats tables

--- admin/partials/manuel-image-stats.php
+++ admin/partials/manuel-image-stats.php
@@ -6,32 +6,121 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
-// Fetch statistics from the database.
-$manuel_settings = new Manuel_Settings( '1.0' );
-$removed_images = $manuel_settings->get_manuel_cron()->get_images_stats();
+if (!class_exists('WP_List_Table')) {
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+}
+
+class Manuel_Image_Stats extends \WP_List_Table
+{
+    public function __construct() {
+        parent::__construct([
+            'singular' => __('Removed Image', 'manuel'),
+            'plural'   => __('Removed Images', 'manuel'),
+            'ajax'     => false
+        ]);
+
+        $this->prepare_items();
+    }
+
+    public function get_columns() {
+        return [
+            'cb'            => '<input type="checkbox" />',
+            'post_id'       => __('Post ID', 'manuel'),
+            'post_title'    => __('Post Title', 'manuel'),
+            'original_image' => __('Original Image', 'manuel'),
+            'time_removed'  => __('Time Removed', 'manuel')
+        ];
+    }
+
+    public function prepare_items() {
+        $perPage = 20;
+        $currentPage = $this->get_pagenum();
+        $manuel_settings = new Manuel_Settings( '1.0' );
+        $data = $manuel_settings->get_manuel_cron()->get_images_stats();
+        
+        $totalItems = count($data);
+
+        $this->set_pagination_args([
+            'total_items' => $totalItems,
+            'per_page'    => $perPage
+        ]);
+
+        $this->items = array_slice($data, (($currentPage-1)*$perPage), $perPage);
+
+        $this->_column_headers = [$this->get_columns(), [], []];
+    }
+
+    public function column_default($item, $column_name) {
+        switch ($column_name) {
+            case 'post_id':
+            case 'post_title':
+            case 'time_removed':
+                return esc_html($item[$column_name]);
+            case 'original_image':
+                return '<a href="' . esc_url($item[$column_name]) . '">' . esc_url($item[$column_name]) . '</a>';
+            default:
+                return print_r($item, true);
+        }
+    }
+
+    public function column_title($item) {
+        $title = '<strong>' . esc_html($item->post_title) . '</strong>';
+
+        $actions = [
+            'edit'      => sprintf('<a href="%s">%s</a>', get_edit_post_link($item->ID), __('Edit', 'manuel')),
+            'trash'     => sprintf('<a href="%s">%s</a>', get_delete_post_link($item->ID), __('Trash', 'manuel')),
+            'view'      => sprintf('<a href="%s">%s</a>', get_permalink($item->ID), __('View', 'manuel'))
+        ];
+
+        return $title . $this->row_actions($actions);
+    }
+
+    public function column_cb($item) {
+        return sprintf('<input type="checkbox" name="bulk-trash[]" value="%s" />', $item->ID);
+    }
+
+    public function extra_tablenav($which) {
+        if ($which === 'top') {
+            ?>
+            <div class="alignleft actions bulkactions">
+                <label for="bulk-action-selector-top" class="screen-reader-text">Select bulk action</label>
+                <select name="action" id="bulk-action-selector-top">
+                    <option value="-1">Bulk Actions</option>
+                    <option value="trash">Move to Trash</option>
+                </select>
+                <input type="submit" id="doaction" class="button action" value                ="Apply">
+            </div>
+            <div class="alignleft actions">
+                <label class="screen-reader-text" for="post-search-input">Search Posts:</label>
+                <input type="search" id="post-search-input" name="s" value="">
+                <input type="submit" id="search-submit" class="button" value="Search Posts">
+            </div>
+            <?php
+        }
+        if ($which === 'bottom'){
+            ?>
+            <div class="alignleft actions bulkactions">
+                <label for="bulk-action-selector-bottom" class="screen-reader-text">Select bulk action</label>
+                <select name="action2" id="bulk-action-selector-bottom">
+                    <option value="-1">Bulk Actions</option>
+                    <option value="trash">Move to Trash</option>
+                </select>
+                <input type="submit" id="doaction2" class="button action" value="Apply">
+            </div>
+            <?php
+        }
+    }
+}
+
+$ImagesTable = new Manuel_Image_Stats();
 ?>
 
 <div class="wrap">
     <h1><?php esc_html_e( 'Removed Images List', 'manuel' ); ?></h1>
-
-    <table class="widefat">
-        <thead>
-            <tr>
-                <th><?php esc_html_e( 'Post ID', 'manuel' ); ?></th>
-                <th><?php esc_html_e( 'Post Title', 'manuel' ); ?></th>
-                <th><?php esc_html_e( 'Original Image', 'manuel' ); ?></th>
-                <th><?php esc_html_e( 'Time Removed', 'manuel' ); ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ( $removed_images as $stat ) : ?>
-                <tr>
-                    <td><?php echo esc_html( $stat['post_id'] ); ?></td>
-                    <td><?php echo esc_html( $stat['post_title'] ); ?></td>
-                    <td><a href="<?php echo esc_url( $stat['original_image'] ); ?>"><?php echo esc_url( $stat['original_image'] ); ?></a></td>
-                    <td><?php echo esc_html( $stat['time_removed'] ); ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+    <form id="posts-filter" method="get">
+        <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+        <?php
+        $ImagesTable->display();
+        ?>
+    </form>
 </div>
--- admin/partials/manuel-links-stats.php
+++ admin/partials/manuel-links-stats.php
@@ -2,38 +2,128 @@
 
 namespace Manuel;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-// Fetch statistics from the database.
-$manuel_settings = new Manuel_Settings( '1.0' );
-$stats = $manuel_settings->get_manuel_cron()->get_stats();
+if (!class_exists('WP_List_Table')) {
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+}
+
+class Manuel_Links_Stats extends \WP_List_Table {
+    public function __construct()
+    {
+        parent::__construct([
+            'singular' => __('Stat', 'manuel'),
+            'plural'   => __('Stats', 'manuel'),
+            'ajax'     => false
+        ]);
+    }
+
+    public function get_columns() {
+        return [
+            'cb'           => '<input type="checkbox" />', // Render a checkbox.
+            'post_title'   => __('Post Title', 'manuel'),
+            'original_link' => __('Original Link', 'manuel'),
+            'anchor_text'  => __('Anchor Text', 'manuel'),
+            'time_removed' => __('Time Removed', 'manuel')
+        ];
+    }
+
+    public function column_cb($item) {
+        return sprintf('<input type="checkbox" name="bulk-delete[]" value="%s" />', $item['ID']);
+    }
+
+    public function prepare_items() {
+        $columns = $this->get_columns();
+        $hidden = array();
+        $sortable = array();
+
+        $this->_column_headers = array($columns, $hidden, $sortable);
+
+        $manuel_settings = new Manuel_Settings('1.0');
+        $data = $manuel_settings->get_manuel_cron()->get_stats();
+
+        $perPage = 20;
+        $currentPage = $this->get_pagenum();
+        $totalItems = count($data);
+
+        $this->set_pagination_args([
+            'total_items' => $totalItems,
+            'per_page'    => $perPage
+        ]);
+
+        $data = array_slice($data, (($currentPage-1)*$perPage), $perPage);
+
+        $this->items = $data;
+    }
+
+    public function column_default($item, $column_name) {
+        switch ($column_name) {
+            case 'post_id':
+            case 'post_title':
+            case 'anchor_text':
+            case 'time_removed':
+                return esc_html($item[$column_name]);
+            case 'original_link':
+                return '<a href="' . esc_url($item[$column_name]) . '">' . esc_url($item[$column_name]) . '</a>';
+            default:
+                return print_r($item, true);
+        }
+    }
+
+    public function column_post_title($item) {
+        $actions = [
+            'edit'      => sprintf('<a href="%s">%s</a>', get_edit_post_link($item['post_id']), __('Edit', 'manuel')),
+            'trash'     => sprintf('<a href="%s">%s</a>', get_delete_post_link($item['post_id']), __('Trash', 'manuel')),
+            'view'      => sprintf('<a href="%s">%s</a>', get_permalink($item['post_id']), __('View', 'manuel'))
+        ];
+
+        return sprintf('%1$s %2$s', $item['post_title'], $this->row_actions($actions));
+    }
+
+    public function extra_tablenav($which) {
+        if ($which === 'top') {
+            ?>
+            <div class="alignleft actions bulkactions">
+                <label for="bulk-action-selector-top" class="screen-reader-text">Select bulk action</label>
+                <select name="action" id="bulk-action-selector-top">
+                    <option value="-1">Bulk Actions</option>
+                    <option value="trash">Move to Trash</option>
+                </select>
+                <input type="submit" id="doaction" class="button action" value                ="Apply">
+            </div>
+            <div class="alignleft actions">
+                <label class="screen-reader-text" for="post-search-input">Search Posts:</label>
+                <input type="search" id="post-search-input" name="s" value="">
+                <input type="submit" id="search-submit" class="button" value="Search Posts">
+            </div>
+            <?php
+        }
+        if ($which === 'bottom'){
+            ?>
+            <div class="alignleft actions bulkactions">
+                <label for="bulk-action-selector-bottom" class="screen-reader-text">Select bulk action</label>
+                <select name="action2" id="bulk-action-selector-bottom">
+                    <option value="-1">Bulk Actions</option>
+                    <option value="trash">Move to Trash</option>
+                </select>
+                <input type="submit" id="doaction2" class="button action" value="Apply">
+            </div>
+            <?php
+        }
+    }
+}
+
+$linksTable = new Manuel_Links_Stats();
 ?>
 
 <div class="wrap">
-    <h1><?php esc_html_e( 'Manuel Stats', 'manuel' ); ?></h1>
-
-    <table class="widefat">
-        <thead>
-            <tr>
-                <th><?php esc_html_e( 'Post ID', 'manuel' ); ?></th>
-                <th><?php esc_html_e( 'Post Title', 'manuel' ); ?></th>
-                <th><?php esc_html_e( 'Original Link', 'manuel' ); ?></th>
-                <th><?php esc_html_e( 'Anchor Text', 'manuel' ); ?></th>
-                <th><?php esc_html_e( 'Time Removed', 'manuel' ); ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ( $stats as $stat ) : ?>
-                <tr>
-                    <td><?php echo esc_html( $stat['post_id'] ); ?></td>
-                    <td><?php echo esc_html( $stat['post_title'] ); ?></td>
-                    <td><a href="<?php echo esc_url( $stat['original_link'] ); ?>"><?php echo esc_url( $stat['original_link'] ); ?></a></td>
-                    <td><?php echo esc_html( $stat['anchor_text'] ); ?></td>
-                    <td><?php echo esc_html( $stat['time_removed'] ); ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+    <h2><?php esc_html_e('Manuel Links Stats', 'manuel') ?></h2>
+    <form method="post">
+        <?php
+        $linksTable->prepare_items();
+        $linksTable->display();
+        ?>
+    </form>
 </div>
