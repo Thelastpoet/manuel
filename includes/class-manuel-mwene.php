64139890eb2ea1fec94468944fe0bffe49128db9
thelastpoet <emcie4@gmail.com>
2023-06-11 13:33:37 +0300

    We can also remove broken featured images

--- includes/class-manuel-mwene.php
+++ includes/class-manuel-mwene.php
@@ -60,6 +60,9 @@ class Manuel_Mwene {
     
                 // Remove broken images
                 $content = $this->remove_broken_images( $content, $post );
+
+                // Remove broken featured image
+                $this->remove_broken_featured_image( $post );
     
                 // Update the post content if changes were made
                 if ( $content !== $post->post_content ) {
@@ -207,6 +210,27 @@ class Manuel_Mwene {
 
         $content = $dom->saveHTML();
         return $content;        
+    }
+
+    // Let us also remove broken featured images
+    private function remove_bkf_images( $post ) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'manuel_removed_images';
+
+        $featured_image_id = get_post_thumbnail_id( $post->ID );
+        $featured_image_url = wp_get_attachment_url( $featured_image_id );
+
+        if ( ! $this->is_valid_url( $featured_image_url ) ) {
+            // Delete the post thumbnail link
+            delete_post_thumbnail( $post->ID );
+
+            // Insert the removed image details into the db
+            $wpdb->insert($table_name, array(
+                'post_id' => $post->ID,
+                'original_image' => $featured_image_url,
+                'time_removed' => current_time( 'mysql' ),
+            ));
+        }
     }
 
     private function is_valid_url( $url ) {
